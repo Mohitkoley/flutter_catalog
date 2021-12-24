a6f5bec12f6f5d9e97dbf8a7b5ceea5cdbf48201
Mohitkoley <mohitkoley5@gmail.com>
2021-12-24 19:09:27 +0530

    removed bannner in  login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,6 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
         child: Container(
           child: const Text(
