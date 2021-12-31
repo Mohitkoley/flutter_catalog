1510796c5d56007a83ade1e8856ce64014d54188
Mohitkoley <mohitkoley5@gmail.com>
2021-12-31 21:38:08 +0530

    completed basic of model

---
+++ lib/models/catalog.dart
@@ -0,0 +1,25 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+
+  final products = [
+    Item(
+      '1',
+      "Iphone 12 Pro",
+      "Apple iphone 12th genration",
+      999,
+      "#33505a",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
+    )
+  ];
+}
